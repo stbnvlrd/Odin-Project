1ac537024a57daa6e99cc0644a50e19ae02cf1a1
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-04-26 11:05:29 -0500

    Solution Repeat String Exercise

--- JavaScript_Exercises/Fundamentals/2 - repeatString/repeatString.js
+++ JavaScript_Exercises/Fundamentals/2 - repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(world, number = 2) {
+    var repeatedString = '';
+    if (number >= 0){
+        for (var i=1; i <= number; i++){
+            repeatedString = world + repeatedString;
+        }
+        
+    } else{
+        repeatedString = 'ERROR'
+    }
+    return repeatedString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
