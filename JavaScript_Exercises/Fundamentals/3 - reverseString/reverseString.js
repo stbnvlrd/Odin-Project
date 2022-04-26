16194187de5dc94d299a10d52291d71b89e9384d
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-04-26 11:05:59 -0500

    Solution Reverse String exercise

--- JavaScript_Exercises/Fundamentals/3 - reverseString/reverseString.js
+++ JavaScript_Exercises/Fundamentals/3 - reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    var reversedWord = '';
+    for(var i=1; i <= word.length; i++){
+        reversedWord = reversedWord + word.charAt(word.length - i);
+    }
+    return reversedWord
 };
 
+console.log(reverseString('hello'));
+console.log(reverseString('hello there'));
+console.log(reverseString('123! abc!'));
+console.log(reverseString(''));
 // Do not edit below this line
 module.exports = reverseString;
