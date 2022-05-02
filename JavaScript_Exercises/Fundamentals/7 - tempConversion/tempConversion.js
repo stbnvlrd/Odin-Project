2ac95bf2a2f4545f3479e182df3e77ce020d857e
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-05-01 19:52:49 -0500

    Solution Temperature Conversion Exercise

--- JavaScript_Exercises/Fundamentals/7 - tempConversion/tempConversion.js
+++ JavaScript_Exercises/Fundamentals/7 - tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(temp_fahrenheit) {
+  return Math.round(((temp_fahrenheit-32)*5/9)*10)/10
 };
 
-const ctof = function() {
-
+const ctof = function(temp_celsius) {
+  return Math.round(((temp_celsius*1.8)+32)*10)/10
 };
 
 // Do not edit below this line
