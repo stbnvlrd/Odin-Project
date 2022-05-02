60fcaf1604daf8e897ef823cd59955a9070b4614
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-05-01 19:52:14 -0500

    Solution Sum All Exercise

--- JavaScript_Exercises/Fundamentals/5 - sumAll/sumAll.js
+++ JavaScript_Exercises/Fundamentals/5 - sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(inicial_num, final_num) {
+    let sum_total
+    if (typeof inicial_num == 'number' && typeof final_num == 'number' && final_num >= 0 && inicial_num >= 0){
+        if(inicial_num < final_num){
+            sum_total = 0;
+            for(var i = inicial_num; i<=final_num; i++ ){
+                sum_total = sum_total + i; 
+            }
+        } else if (inicial_num > final_num){
+            sum_total = 0;
+            for(var i = final_num; i<=inicial_num; i++ ){
+                sum_total = sum_total + i; 
+            }
+        } else {
+            sum_total = inicial_num
+        }
+    } else {
+        sum_total = 'Error'
+    }
+    return sum_total
 };
 
 // Do not edit below this line
