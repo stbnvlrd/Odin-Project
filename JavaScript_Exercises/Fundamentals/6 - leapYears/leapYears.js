86aac1de282286adff4ccd772cf9197087ebc295
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-05-01 19:52:28 -0500

    Solution Leap Years Exercise

--- JavaScript_Exercises/Fundamentals/6 - leapYears/leapYears.js
+++ JavaScript_Exercises/Fundamentals/6 - leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    var leap = false;
+    if(year % 4 === 0){
+        if(year % 100 === 0){
+            if(year % 400 === 0){
+                leap = true;
+            }
+                
+        } else{
+            leap = true;
+        }
+    } else{
 
+    }
+    return leap
 };
 
 // Do not edit below this line
